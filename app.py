182a15252e4ee8b9b847d55f9a798860154871c0
Luiz Dalla <40028036+luizdalla@users.noreply.github.com>
2023-04-15 13:01:08 -0600

    real state added

--- app.py
+++ app.py
@@ -1,31 +1,72 @@
+#%%
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-app = dash.Dash()
+import dash_bootstrap_components as dbc
+
+import os
+import pandas as pd
+
+import plotly.express as px
+
+#%%
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+df = pd.read_csv(os.path.join(dir_path, 'data', 'df_real_state.csv'))
+
+
+fig = px.box(df, y='price', x='bedrooms',)
+fig.update(layout_yaxis_range = [0,10e6])
+fig.update(layout_xaxis_range = [0,6.5])
+fig.update_layout(
+    height=1000,
+    paper_bgcolor="LightSteelBlue",
+)
+#%%
+
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 application = app.server
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="NavbarSimple",
+    brand_href="#",
+    color="primary",
+    dark=True,
+)
 
-    html.Div(children='''
+
+app.layout = html.Div(children=[
+    navbar,
+    
+    html.H1(children='Vitoria - Real State'),
+
+    html.Div(children='''ß
         This is Dash running on Azure App Service.
     '''),
+    
 
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    )
+    dcc.Graph(figure=fig),
 ])
 
+
+
+#%%
 server = app.server
 
 if __name__ == '__main__':
